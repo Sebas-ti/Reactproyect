87cc191b5f9405994457f88a0d44e7f453168eba
sebastian rey <182707930+Sebas-ti@users.noreply.github.com>
2025-07-14 20:05:29 -0500

    Se agrego texto al componente de proectos el cual describe lo que hace cada proyecto y se exporto el componenete a app.js ademas en app.js se hizo la importacion del componenete como la utilizacion e el componenete proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Proyectos from './componentes/Proyectos.jsx'
 
 function App() {
   return (
     <div className="App">
-      
+      <Proyectos/>
     </div>
   );
 }
--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -2,13 +2,23 @@ import React from "react";
 
 function Proyectos() {
   return(
-    <div>
+    <div className="Contenedor-Proyectos">
+
       <img 
       className="Imagen-Proyectos" 
       src={require("../imagenes/Proyecto-ganadero.png")}
       alt="Imagen del 1 proyecto" 
       />
 
+    <div className="Contenedor-Texto-Proyectos">
+
+      <p className="Titulo-Proyectos">Gestion Ganadera "Animal Management"</p>
+      <p className="Herramientos-usadas">HTML,CSS,JavaScript,Bootstrap,PHP,MariaBD y XAMPP</p>
+      <p className="Descripcion_Proyecto">Lorem ipsum dolor sit amet consectetur adipiscing elit nec cum dictumst nulla vehicula morbi est magna, nam enim nullam primis aliquet maecenas hendrerit class venenatis curae nibh vivamus proin. Mauris habitasse sed class nam justo mollis, id purus accumsan consequat etiam. Proin vehicula urna egestas lectus eros felis tortor at, id elementum facilisi mus hac eleifend nibh, venenatis mauris ac fringilla volutpat sapien iaculis.</p>
+    </div>
+
     </div>
   )
 }
+
+export default Proyectos;
