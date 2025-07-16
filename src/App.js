aebe4edf6f88985f2f06010c5331cfa2042b962d
sebastian rey <182707930+Sebas-ti@users.noreply.github.com>
2025-07-15 22:08:50 -0500

    Se añadio en app.css los estilos de las tarjetas en app.js se pusieron los props de las tarjetas en la carpeta de images se agrego la imagen de santosweed y el proyectos.jsx se modifico el componenete para que se pudieron colocar los props de manera optima

--- src/App.js
+++ src/App.js
@@ -8,8 +8,21 @@ function App() {
       <div className='contenedor-principal'>
         <h1> Mi portafolio </h1>
       </div>
-      <Proyectos/>
-    </div>
+      <Proyectos
+      Imagen="ganadero"
+      NombreProyecto="Animal Management"
+      HerramientasUsadas="HTML,CSS,JavaScript,Bootstrap,PHP,Mysql Y Xampp"
+      DescripcionProyecto="Este proyecto es un sistema de gestión ganadera desarrollado como una aplicación web. La plataforma ofrece información detallada sobre el software e incluye funcionalidades clave como registro de usuarios, inicio de sesión y recuperación de contraseña. Una vez autenticados, los usuarios acceden a un panel principal (dashboard) donde pueden registrar, modificar y eliminar información relacionada con los animales. Los datos gestionados abarcan aspectos como salud, inventarios, reproducción y administración de fincas"
+      />
+      <Proyectos
+      Imagen="SantosWeed" 
+      NombreProyecto="SantosWeed"
+      HerramientasUsadas="Herramientas del Segundo Proyecto"
+      DescripcionProyecto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor enim sit amet massa bibendum gravida. Proin vehicula odio sit amet massa tincidunt congue. Suspendisse eu sapien gravida, aliquet nibh ac, auctor magna. Morbi porta sem ut mauris iaculis dictum. Nullam congue ligula eu feugiat laoreet. Duis enim ex, condimentum vel turpis sit amet, dictum cursus magna. Sed urna lorem, pharetra non elit ac, cursus bibendum metus. Quisque nec aliquam eros.."
+      />
+
+
+      </div>
   );
 }
 
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import '../stylesheets/Proyectos.css'
 
-function Proyectos() {
+function Proyectos(props) {
   return(
     <div className="Contenedor-Proyectos">
 
       <img 
       className="Imagen-Proyectos" 
-      src={require("../images/Proyecto-ganadero.png")}
+      src={require(`../images/Proyecto-${props.Imagen}.png`)}
       alt="Imagen del 1 proyecto" 
       />
 
     <div className="Contenedor-Texto-Proyectos">
 
-      <p className="Titulo-Proyectos">Gestion Ganadera "Animal Management"</p>
-      <p className="Herramientos-usadas">HTML,CSS,JavaScript,Bootstrap,PHP,MariaBD y XAMPP</p>
-      <p className="Descripcion_Proyecto">Lorem ipsum dolor sit amet consectetur adipiscing elit nec cum dictumst nulla vehicula morbi est magna, nam enim nullam primis aliquet maecenas hendrerit class venenatis curae nibh vivamus proin. Mauris habitasse sed class nam justo mollis, id purus accumsan consequat etiam. Proin vehicula urna egestas lectus eros felis tortor at, id elementum facilisi mus hac eleifend nibh, venenatis mauris ac fringilla volutpat sapien iaculis.</p>
+      <p className="Titulo-Proyectos">{props.NombreProyecto}</p>
+      <p className="Herramientas-usadas">{props.HerramientasUsadas}</p>
+      <p className="Descripcion_Proyecto">{props.DescripcionProyecto}</p>
     </div>
 
     </div>
