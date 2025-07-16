4fc44f8bd7a575c2b119bec1088b851cfd4f62e7
sebastian rey <182707930+Sebas-ti@users.noreply.github.com>
2025-07-15 22:30:01 -0500

    Se agrego en components un nuevo componenete llamado YouTube el cual carga videos de youtube ademas en App.js se agrego el componenete de YouTube

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Proyectos from './components/Proyectos.jsx';
+import YouTube from './components/YouTube.jsx';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
       HerramientasUsadas="HTML,CSS,JavaScript,Bootstrap,PHP,Mysql Y Xampp"
       DescripcionProyecto="Este proyecto es un sistema de gestión ganadera desarrollado como una aplicación web. La plataforma ofrece información detallada sobre el software e incluye funcionalidades clave como registro de usuarios, inicio de sesión y recuperación de contraseña. Una vez autenticados, los usuarios acceden a un panel principal (dashboard) donde pueden registrar, modificar y eliminar información relacionada con los animales. Los datos gestionados abarcan aspectos como salud, inventarios, reproducción y administración de fincas"
       />
+
+      <h1>Mi Aplicación de Videos de YouTube</h1>
+      <YouTube videoId="dQw4w9WgXcQ" /> 
+
       <Proyectos
       Imagen="SantosWeed" 
       NombreProyecto="SantosWeed"
---
+++ src/components/YouTube.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function YouTube({ videoId, title = 'YouTube video player' }) {
+  // Construye la URL de incrustación de YouTube
+  const embedUrl = `http://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-container" style={{
+      position: 'relative',
+      width: '100%',
+      paddingBottom: '56.25%', /* 16:9 Aspect Ratio */
+      height: 0,
+      overflow: 'hidden',
+      maxWidth: '100%',
+      background: '#000'
+    }}>
+      <iframe
+        src={embedUrl}
+        title={title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+        }}
+      ></iframe>
+    </div>
+  );
+}
+
+export default YouTube;
